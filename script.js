33065b4dd6e3848cba3c25639961b4054057bf72
Joshua Rosado Olivencia <joshua06rosado@gmail.com>
2022-12-24 09:47:53 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+
+function hide(element) {
+    element.remove(element);
+}
+function like (id) {
+    console.log ("liked")
+    document.getElementById(id).innerHTML++; 
+}
+function choice2(select) {
+    alert('You are looking for a Cat');
+    console.log(select);
+} 
+function choice3(select) {
+    alert('You are looking for Dog');
+    console.log(select);
+}
+
